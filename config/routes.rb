a16377bb7083bd1a632fb5e7f721cc0d61aa750d
TJ Muehleman <tj.muehleman@gmail.com>
2014-02-19 10:29:19 -0500

    scaffolds added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TeamGenerator::Application.routes.draw do
+  resources :teams
+
+  resources :students
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
