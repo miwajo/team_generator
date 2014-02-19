1e75210ea10fb1c064d39fc04916bca02a88190f
TJ Muehleman <tj.muehleman@gmail.com>
2014-02-19 10:20:42 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamGenerator::Application.config.secret_key_base = 'd3e5b4d0dcc6f45a5f1c46837d43d8e2bdd4ea5dd95015d87a0da251e05373e0d1985a4d9102dc94d8d71ec8e5031c85178addc3e019b67f6221775e3fd6d9f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeamGenerator::Application.config.session_store :cookie_store, key: '_team_generator_session'
